09f04ec36f24456bf7ec3b7115a72d6f02b620a6
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2018-02-02 13:35:14 +0100

    added upload of the results to S3 if enough info provided; minor refactors.

--- app/reducer/deployment.sh
+++ app/reducer/deployment.sh
@@ -11,21 +11,19 @@ echo "@REDUCER_RUN: "$(timestamp)" - \
           VM started on cloudservice: `ss-get cloudservice` \
           with service-offer: `ss-get service-offer`."
 
-S3_HOST=`ss-get --noblock s3-host`
-S3_BUCKET=`ss-get --noblock s3-bucket`
-S3_ACCESS_KEY=`ss-get --noblock s3-access-key`
-S3_SECRET_KEY=`ss-get --noblock s3-secret-key`
+mapper_ids=`ss-get --noblock mapper:ids | sed -e 's/,/ /g'`
+mapper_mult=`ss-get mapper:multiplicity`
 
 set_listeners() {
     # Lauch netcat daemons for each  product
-    echo -n  $@ | xargs -d ' ' -I% bash -c '(nc -l 808%  0<&- 1>%.png) &'
+    echo -n $mapper_ids | xargs -d ' ' -I% bash -c '(nc -l 808%  0<&- 1>%.png) &'
 }
 
 check_mappers_ready() {
     # Run multiple daemons depending on the mapper VM multiplicity and
     # whithin these a timeout checking mapper's ready state is triggered.
 
-    echo -n $ids | xargs -d ' ' -I% bash -c '(
+    echo -n $mapper_ids | xargs -d ' ' -I% bash -c '(
     ss-get --timeout 2600 mapper.%:ready
     echo 'mapper.'%':ready' >>readylock.md
     exit 0) &'
@@ -39,30 +37,51 @@ count_ready() {
 
 wait_mappers_ready() {
     touch readylock.md
-    ids=`ss-get --noblock mapper:ids | sed -e 's/,/ /g'`
-    set_listeners $ids
     check_mappers_ready
     # Wait before all mappers are in ready state.
-    mapper_mult=`ss-get mapper:multiplicity`
     while [ $(count_ready) -ne $mapper_mult ]; do
         sleep 100
     done
 }
+
+# Clone reducer.
+git clone `ss-get proc-git-repo` $REDUCER_LOC
 
 # Clone itself.
 git clone https://github.com/SixSq/SAR-app.git $SARAPP_LOC
 cd $SARAPP_LOC/app/reducer
 source ../lib.sh
 start_filebeat
+set_listeners
 cd $SARAPP_LOC/app/reducer
 wait_mappers_ready
 
-# Clone reducer.
-git clone `ss-get proc-git-repo` $REDUCER_LOC
-
 # Generate the final output
 cd $REDUCER_LOC
-echo "@REDUCER_RUN :"$(timestamp): "Start conversion."
-export PROCESSED_DATA_LOC=$SARAPP_LOC/app/reducer
-./SAR_reducer.sh
-echo "@REDUCER_RUN :"$(timestamp): "Finish conversion."
+echo "@REDUCER_RUN $(timestamp) start conversion."
+export INPUT_DATA_LOC=$SARAPP_LOC/app/reducer
+export OUTPUT_DATA_LOC=$SARAPP_LOC/app/reducer/output
+mkdir -p $OUTPUT_DATA_LOC
+./SAR_reducer.sh $INPUT_DATA_LOC $OUTPUT_DATA_LOC
+echo "@REDUCER_RUN $(timestamp) finish conversion."
+
+# Upload result to user defined object store.
+S3_HOST=`ss-get --noblock s3-host`
+S3_BUCKET=`ss-get --noblock s3-bucket`
+S3_ACCESS_KEY=`ss-get --noblock s3-access-key`
+S3_SECRET_KEY=`ss-get --noblock s3-secret-key`
+if [ -n "$S3_HOST" -a -n "$S3_BUCKET" -a -n "$S3_ACCESS_KEY" -a -n "$S3_SECRET_KEY" ]; then
+    cat > ~/.s3cfg <<EOF
+[default]
+host_base = $S3_HOST
+host_bucket = %(bucket)s.$S3_HOST
+access_key = $S3_ACCESS_KEY
+secret_key = $S3_SECRET_KEY
+use_https = True
+EOF
+    echo "@REDUCER_RUN $(timestamp) start uploading result(s) to S3."
+    s3cmd put $OUTPUT_DATA_LOC/* s3://$S3_BUCKET
+    echo "@REDUCER_RUN $(timestamp) finish uploading result(s) to S3."
+else
+    echo "WARNING: Not uploading result to S3. Not all S3 options were provided."
+fi
